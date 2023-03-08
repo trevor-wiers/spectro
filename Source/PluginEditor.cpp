b8ae661cc491fe7a2717260cc376a849df76673e
Trevor Wiers <trevor.t.wiers@gmail.com>
2023-03-08 12:15:25 -0600

    transfered over tut code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -11,11 +11,14 @@
 
 //==============================================================================
 SpectroAudioProcessorEditor::SpectroAudioProcessorEditor (SpectroAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+    : AudioProcessorEditor (&p), audioProcessor (p), forwardFFT (fftOrder), spectrogramImage (juce::Image::RGB, 512, 512, true)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setOpaque(true);
+    startTimerHz(60);
+    
+    setSize (700, 500);
 }
 
 SpectroAudioProcessorEditor::~SpectroAudioProcessorEditor()
@@ -26,15 +29,66 @@ SpectroAudioProcessorEditor::~SpectroAudioProcessorEditor()
 void SpectroAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (juce::Colours::black);
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.setOpacity (1.0f);
+    g.drawImage (spectrogramImage, getLocalBounds().toFloat());
+
 }
 
 void SpectroAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+}
+
+void SpectroAudioProcessorEditor::timerCallback()
+{
+    if (nextFFTBlockReady)
+    {
+        drawNextLineOfSpectrogram();
+        nextFFTBlockReady = false;
+        repaint();
+    }
+}
+
+void SpectroAudioProcessorEditor::pushNextSampleIntoFifo (float sample) noexcept
+{
+    // if the fifo contains enough data, set a flag to say
+    // that the next line should now be rendered..
+    if (fifoIndex == fftSize)
+    {
+        if (! nextFFTBlockReady)
+        {
+            std::fill (fftData.begin(), fftData.end(), 0.0f);
+            std::copy (fifo.begin(), fifo.end(), fftData.end());
+            nextFFTBlockReady = true;
+        }
+        fifoIndex = 0;
+    }
+    fifo[(size_t) fifoIndex++] = sample;
+}
+
+void SpectroAudioProcessorEditor::drawNextLineOfSpectrogram()
+{
+    auto rightHandEdge = spectrogramImage.getWidth() - 1;
+    auto imageHeight = spectrogramImage.getHeight();
+    
+    //first shuffle image foward
+    spectrogramImage.moveImageSection(0, 0, 1, 0, rightHandEdge, imageHeight);
+    
+    //render FFT data
+    forwardFFT.performFrequencyOnlyForwardTransform(fftData.data());
+    
+    // find value range to scale
+    auto maxLevel = juce::FloatVectorOperations::findMinAndMax (fftData.data(), fftSize / 2);
+    
+    for (auto y = 1; y < imageHeight; ++y)
+    {
+        auto skewedProportionY = 1.0f - std::exp (std::log ((float) y / (float) imageHeight) * 0.2f);
+        auto fftDataIndex = (size_t) juce::jlimit (0, fftSize / 2, (int) (skewedProportionY * fftSize / 2));
+        auto level = juce::jmap (fftData[fftDataIndex], 0.0f, juce::jmax (maxLevel.getEnd(), 1e-5f), 0.0f, 1.0f);
+        
+        spectrogramImage.setPixelAt (rightHandEdge, y, juce::Colour::fromHSV (level, 1.0f, level, 1.0f));
+    }
 }
