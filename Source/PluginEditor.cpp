f3d2b0d5b3020d024d505e5b415682e8687f438e
Trevor Wiers <trevor.t.wiers@gmail.com>
2023-03-14 15:40:56 -0500

    second drawing method

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -9,6 +9,8 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+#include <math.h>
+
 //==============================================================================
 SpectroAudioProcessorEditor::SpectroAudioProcessorEditor (SpectroAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p), spectrogramImage (juce::Image::RGB, 512, 512, true)
@@ -60,15 +62,39 @@ void SpectroAudioProcessorEditor::drawNextLineOfSpectrogram()
     //first shuffle image foward
     spectrogramImage.moveImageSection(0, 0, 1, 0, rightHandEdge, imageHeight);
     
-    // find value range to scale
-    auto maxLevel = juce::FloatVectorOperations::findMinAndMax (audioProcessor.fftData.data(), audioProcessor.fftSize / 2);
+    //first drawing method
+//     find value range to scale
+//    auto maxLevel = juce::FloatVectorOperations::findMinAndMax (audioProcessor.fftData.data(), audioProcessor.fftSize / 2);
+//
+//    for (auto y = 1; y < imageHeight; ++y)
+//    {
+//        auto skewedProportionY = 1.0f - std::exp (std::log ((float) y / (float) imageHeight) * 0.2f);
+//        auto fftDataIndex = (size_t) juce::jlimit (0, audioProcessor.fftSize / 2, (int) (skewedProportionY * audioProcessor.fftSize / 2));
+//        auto level = juce::jmap (audioProcessor.fftData[fftDataIndex], 0.0f, juce::jmax (maxLevel.getEnd(), 1e-5f), 0.0f, 1.0f);
+//
+//        spectrogramImage.setPixelAt (rightHandEdge, y, juce::Colour::fromHSV (.5f, (1.0f - level), level, 1.0f));
+//    }
     
-    for (auto y = 1; y < imageHeight; ++y)
-    {
-        auto skewedProportionY = 1.0f - std::exp (std::log ((float) y / (float) imageHeight) * 0.2f);
-        auto fftDataIndex = (size_t) juce::jlimit (0, audioProcessor.fftSize / 2, (int) (skewedProportionY * audioProcessor.fftSize / 2));
-        auto level = juce::jmap (audioProcessor.fftData[fftDataIndex], 0.0f, juce::jmax (maxLevel.getEnd(), 1e-5f), 0.0f, 1.0f);
-        
-        spectrogramImage.setPixelAt (rightHandEdge, y, juce::Colour::fromHSV (.5f, 1.0f, level, 1.0f));
-    }
+    // second drawing method
+//    std::array<float, 512> newBins;
+//    std::fill (newBins.begin(), newBins.end(), 0.0f);
+//
+//    for (auto i = 1; i < (audioProcessor.fftSize / 2) + 1; ++i)
+//    {
+//        float skewed = log2(i);
+//        int bin = (int) (juce::jmap(skewed, 0.f, log2(audioProcessor.fftSize / 2.f), 1.f, 512.f) - 1.f);
+//        float level = sqrt(audioProcessor.fftData[i - 1]) / 50;
+//        newBins[bin] += level;
+//    }
+//
+//    for (auto i = 0; i < imageHeight; ++i)
+//    {
+//        float level = newBins[i];
+//        level = juce::jlimit (0.f, 1.f, (float) level);
+//        spectrogramImage.setPixelAt (rightHandEdge, imageHeight - i + 1, juce::Colour::fromHSV (.5f, 1.0f, level, 1.0f));
+//    }
+    
+    //third drawing method
+    
+    
 }
